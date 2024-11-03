c9f9a6dd121ee899cfd703e801ca746e8881b633
kgsng096 <keithng.096@gmail.com>
2024-11-04 07:51:04 +0800

    sign up feature

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -5,6 +5,7 @@ const SignUpPage = () => {
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center ">
       <h1 className="text-center text-2xl font-bold mb-4">Sign Up Page</h1>
+      <SignUpPage />
       <div className="flex justify-between text-sm">
         <p>Already have an account?</p>
         <Link className="underline" href={"/auth/signin"}>
